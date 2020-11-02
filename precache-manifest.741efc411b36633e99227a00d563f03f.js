0a6d8c8061aed9aec0b66ce490d7269a7e44bf81
[aliciafl] <alicia.fl@outlook.com>
2020-11-02 15:47:16 +0200

    Updates

---
+++ precache-manifest.741efc411b36633e99227a00d563f03f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6775b35e4a1770f27f27b977663c8a8",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "d7f2f675647f4a58282a",
+    "url": "/website/static/css/main.fa027cf1.chunk.css"
+  },
+  {
+    "revision": "5dec771342bda220ffa9",
+    "url": "/website/static/js/2.066948b3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/website/static/js/2.066948b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f2f675647f4a58282a",
+    "url": "/website/static/js/main.33eeaa06.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
